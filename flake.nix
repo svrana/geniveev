d97d5abb66c35b0b818bbe9e7c84b94101b9f081
Shaw Vrana <shaw@vranix.com>
2023-08-29 15:07:57 -0700

    Add default package to flake
    So we can pull the package in more easily from other projects since
    it's not in nixpkgs.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,38 @@
 {
   description = "geniveev dev shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+    gomod2nix = {
+      url = "github:tweag/gomod2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.utils.follows = "utils";
+    };
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, utils, gomod2nix }:
+    utils.lib.eachDefaultSystem (system:
+    let pkgs =  import nixpkgs {
+      inherit system;
+      overlays = [
+        gomod2nix.overlays.default
+      ];
+    };
     in {
+      packages.default = pkgs.buildGoApplication {
+        pname = "geniveev";
+        version = "0.1.1";
+        pwd = ./.;
+        src = ./.;
+        modules = ./gomod2nix.toml;
+      };
+
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.bashInteractive
           pkgs.go_1_20
           pkgs.goreleaser
+          gomod2nix.packages.${system}.default
         ];
       };
     });
